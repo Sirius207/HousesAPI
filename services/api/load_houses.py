bbb226b08e328a2ea8142da35769bbdbdd1fd734
sirius <oceanus11034@gmail.com>
2021-06-05 13:33:20 +0800

    feat: use house_id as collections pkey

--- services/api/load_houses.py
+++ services/api/load_houses.py
@@ -1,5 +1,6 @@
 from mongoengine import connect
 import pandas as pd
+from loguru import logger
 
 from endpoints.houses.model import House
 from config import Config
@@ -15,5 +16,13 @@ connect(
 
 df = pd.read_csv("data/temp_info.csv")
 
-houses = [House(**house_data) for house_data in df.to_dict("records")]
+df["house_id"] = df["url"].map(
+    lambda url: url.replace("https://rent.591.com.tw/rent-detail-", "")[:-5]
+)
+df = df.drop(["url"], axis=1)
+
+unique_df = df.drop_duplicates()
+logger.info(f"Origin: {len(df)}, Drop Duplicates: {len(unique_df)}")
+
+houses = [House(**house_data) for house_data in unique_df.to_dict("records")]
 House.objects.insert(houses)
